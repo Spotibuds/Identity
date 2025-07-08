e186a230b5b4ee79cb3a09d24fd7fc462420a4bc
Catmanaha <dadashov.tamerlan@gmail.com>
2025-07-08 22:24:31 +0400

    New web app

--- Program.cs
+++ Program.cs
@@ -92,6 +92,8 @@ builder.Services.AddAuthorization();
 builder.Services.AddHttpClient<IUserSyncService, UserSyncService>();
 builder.Services.AddScoped<IUserSyncService, UserSyncService>();
 
+//listen port 80
+
 builder.WebHost.UseUrls("http://0.0.0.0:80");
 
 var app = builder.Build();
